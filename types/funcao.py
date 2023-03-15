ba9c318e4a85c855fdd21d994b2cfda21386bfe4
valkenrox <soloqvalken@gmail.com>
2023-03-14 21:17:24 -0300

    new file:   types/funcao.py

---
+++ types/funcao.py
@@ -0,0 +1,41 @@
+"""
+Escopo de funções em Python
+Escopo significa o local onde aquele código pode atingir.
+Existe o escopo global e local.
+O escopo global é o escopo onde todo o código é alcançavel.
+O escopo local é o escopo onde apenas nomes do mesmo local
+podem ser alcançados.
+"""
+
+x = 1
+
+
+def escopo():
+    global x
+    x = 10
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
+    print(x)
+
+
+print(x)
+escopo()
+print(x)
+
+def soma(x,y):
+    soma = x + y 
+    return soma
+
+soma2 = soma(6,2)
+
+soma3 = soma(3,9)
+
+soma4 = soma2 + soma3
+
+print(soma4)
