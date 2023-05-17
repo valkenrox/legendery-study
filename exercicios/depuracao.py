f93985ed1eea87f31e5828295d87493bad8c4da3
valkenrox <soloqvalken@gmail.com>
2023-05-17 19:18:44 -0300

    new file:   exercicios/depuracao.py 	types/prog_funcional.py

---
+++ exercicios/depuracao.py
@@ -0,0 +1,21 @@
+# num = input("Digite um número: ")
+# if int(num) % 2 == 0:
+#     print("O número é par.")
+# else:
+#     print("O número é ímpar.")
+
+# numbers = input("Digite uma lista de números separados por vírgula: ")
+# numbers_list = numbers.split(",")
+# print(numbers_list)
+# numbers_list.sort()
+# print(numbers_list[0])
+
+# word1 = input("Digite a primeira palavra: ")
+# word2 = input("Digite a segunda palavra: ")
+# print(word1[0], word2[0])
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(i * j, end="\t")
+#     print("\n")
+
