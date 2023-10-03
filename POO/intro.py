5e4be45e1a6e77a0dfd1c499367e986cb69be6ec
valkenrox <85355194+valkenrox@users.noreply.github.com>
2023-10-02 22:28:21 -0300

    new file:   POO/intro.py 	modified:   POO/intro.py

---
+++ POO/intro.py
@@ -0,0 +1,44 @@
+#POO é um paradigma de programação que utiliza de classes e objetos para modelar um problema
+#Dividos em classes e objetos, classes que simulam objetos do mundo real, em um nivel de abstração
+#e objetos que são instancias de classes, que simulam comportamentos e atividades
+#Um objeto é uma abstração de algum fato ou ente do mundo real, com atributos que representam as suas caraterísticas ou propriedades
+#e métodos que representam as suas atividades ou ações
+
+
+#Criando uma classe
+class Pessoa:
+    #Atributos
+    nome = None
+    idade = None
+    altura = None
+    peso = None
+
+    #Métodos
+    def andar(self):
+        print("A pessoa está andando")
+
+    def comer(self):
+        print("A pessoa está comendo")
+
+    def dormir(self):
+        print("A pessoa está dormindo")
+
+#Criando um objeto
+pessoa1 = Pessoa()
+pessoa1.nome = "João"
+pessoa1.idade = 20
+pessoa1.altura = 1.75
+pessoa1.peso = 70
+
+#Acessando os atributos
+print(pessoa1.nome)
+print(pessoa1.idade)
+print(pessoa1.altura)
+print(pessoa1.peso)
+
+
+pessoa1.andar()
+
+
+
+
